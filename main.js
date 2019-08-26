750571a0fac1ec1fd341c0b856ffcdba213993da
Heidi Smith <Heidi@LKs-MacBook-Pro.local>
2019-08-26 16:10:33 -0500

    practice4-chickenMonkey

---
+++ main.js
@@ -0,0 +1,15 @@
+
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+    if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+        console.log("chickenMonkey");}
+        else if (currentNumber % 5 === 0) {
+            console.log("chicken");
+        }else if(currentNumber % 7 === 0) {
+            console.log("Monkey");
+        }else {
+            console.log(currentNumber);
+        }
+
+        }
+       
+    
